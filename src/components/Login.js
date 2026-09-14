5fb1cab2059d09b4286ef3c29aba935bb064d2d6
Niharika-DSA <niharika.prashanth4827@gmail.com>
2026-09-14 10:41:26 +0530

    Remove unused useNavigate import

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Login() {
   const [email, setEmail] = useState('');
